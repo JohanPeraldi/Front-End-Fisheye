9abed7f406414db4e1826b5e38b0802c3157f01a
Johan <johan.peraldi@gmail.com>
2022-05-25 09:39:02 +0200

    Get all photographer data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-  const {name, portrait} = data;
+  const {name, id, city, country, tagline, price, portrait} = data;
 
   const picture = `assets/photographers/${portrait}`;
 
@@ -11,6 +11,13 @@ function photographerFactory(data) {
     h2.textContent = name;
     article.appendChild(img);
     article.appendChild(h2);
+    console.group('Photographer data');
+    console.log('id: ' + id);
+    console.log('city: ' + city);
+    console.log('country: ' + country);
+    console.log('tagline: ' + tagline);
+    console.log('price: ' + price + '€/jour');
+    console.groupEnd();
     return (article);
   }
 
