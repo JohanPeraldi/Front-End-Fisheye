55bbe607618bdc1aac5f58beb2fecd38fbfe68bf
Johan <johan.peraldi@gmail.com>
2022-05-29 14:32:19 +0200

    Add contact form modal

--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -1,11 +1,5 @@
 // DOM elements
 const pageTitleElement = document.querySelector('title');
-// const contactButton = document.getElementById('contact_button');
-// const closeModalIcon = document.getElementById('close-modal');
-
-// Events
-// contactButton.addEventListener('click', () => displayModal());
-// closeModalIcon.addEventListener('click', () => closeModal());
 
 // Update page title
 let photographerName = 'Update me';
@@ -13,7 +7,6 @@ let photographerName = 'Update me';
 // Get id of current photographer
 let params = (new URL(document.location)).searchParams;
 let id = params.get('id');
-console.log('Photographer id: ' + id);
 
 // Get photographer data
 async function getPhotographer() {
@@ -21,16 +14,6 @@ async function getPhotographer() {
   const response = await data.get();
   response.photographers.forEach(photographer => {
     if (photographer.id === +id) {
-      console.group('Photographer data');
-      console.log('Name: ' + photographer.name);
-      console.log('Id: ' + photographer.id);
-      console.log('City: ' + photographer.city);
-      console.log('Country: ' + photographer.country);
-      console.log('Tagline: ' + photographer.tagline);
-      console.log('Rate: ' + photographer.price + '€/day');
-      console.log('Image file: ' + photographer.portrait);
-      console.log('Photographer: ' + photographer);
-      console.groupEnd();
       photographerName = photographer.name;
       pageTitleElement.textContent = 'Fisheye - ' + photographerName;
 
@@ -46,6 +29,14 @@ async function getPhotographer() {
 async function displayPhotographerData(photographer) {
   const photographerCard = document.querySelector('.photographer__header');
   photographerCard.innerHTML = new PhotographerCard(photographer, 'photographer').createPhotographerCard();
+
+  // Target DOM elements created above
+  const contactButton = document.getElementById('contact_button');
+  const closeModalIcon = document.getElementById('close-modal');
+
+  // Events
+  contactButton.addEventListener('click', () => displayModal());
+  closeModalIcon.addEventListener('click', () => closeModal());
 }
 
 getPhotographer();
