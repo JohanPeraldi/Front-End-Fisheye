572524d362c8dcbb1f8b6d54e0161e9c968eae48
Johan <johan.peraldi@gmail.com>
2022-06-09 14:26:12 +0200

    Correct path

--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -54,7 +54,7 @@ const getPhotographers = () => {
    * @type {PhotographersApi}
    * @return A method to fetch photographer objects
    */
-  const photographersApiInstance = new PhotographersApi('data/photographers.json');
+  const photographersApiInstance = new PhotographersApi('/data/photographers.json');
   return photographersApiInstance.getPhotographers();
 };
 
