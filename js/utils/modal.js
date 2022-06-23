38505d7b3a50b72bb48b09a60ffaee0a6891af80
Johan <johan.peraldi@gmail.com>
2022-06-23 16:55:41 +0200

    Remove no-scroll class to allow full access to contact form on screens with small viewport height

--- js/utils/modal.js
+++ js/utils/modal.js
@@ -1,12 +1,10 @@
 // DOM elements
-const bodyElement = document.querySelector('body');
 const contentWrapperElement = document.getElementById('content-wrapper');
 const modal = document.getElementById('contact-modal');
 const closeButtonElement = document.getElementById('close-modal');
 
 // Open modal
 const displayModal = () => {
-  bodyElement.classList.add('no-scroll');
   contentWrapperElement.setAttribute('aria-hidden', 'true');
   modal.style.display = 'block';
   modal.setAttribute('aria-hidden', 'false');
@@ -15,7 +13,6 @@ const displayModal = () => {
 
 // Close modal
 const closeModal = () => {
-  bodyElement.classList.remove('no-scroll');
   contentWrapperElement.setAttribute('aria-hidden', 'false');
   modal.style.display = 'none';
   modal.setAttribute('aria-hidden', 'true');
